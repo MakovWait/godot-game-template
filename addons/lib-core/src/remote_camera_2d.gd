25a6bc240d1d06f7550e8222708def4a73695b45
MakovWait <maxkv258@gmail.com>
2025-05-14 15:59:18 +0200

    rotation in remote_camera_2d

--- addons/lib-core/src/remote_camera_2d.gd
+++ addons/lib-core/src/remote_camera_2d.gd
@@ -12,3 +12,6 @@ func _process(delta: float) -> void:
 	_target.global_transform = _origin.global_transform
 	_target.offset = _origin.offset
 	_target.zoom = _origin.zoom
+	
+	_target.ignore_rotation = _origin.ignore_rotation
+	_target.rotation = _origin.rotation
