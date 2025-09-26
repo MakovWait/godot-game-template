a8500fbf7c231e6966f2d4e2b7babbb4dd1a5e6b
MakovWait <maxkv258@gmail.com>
2025-09-26 21:59:55 +0200

    #43

--- addons/lib-core/src/utils/default_hooks.gd
+++ addons/lib-core/src/utils/default_hooks.gd
@@ -1,11 +1,21 @@
 static func use_stopwatch(parent: Node) -> Stopwatch:
 	var stopwatch := Stopwatch.new()
 	parent.add_child(stopwatch)
+	stopwatch.owner = parent
 	return stopwatch
 
 
 static func use_timer(node: Node) -> Timer:
 	var timer := Timer.new()
+	node.add_child(timer)
+	timer.owner = node
+	return timer
+
+
+static func use_oneshot_timer(node: Node) -> Timer:
+	var timer := Timer.new()
+	timer.one_shot = true
+	timer.owner = node
 	node.add_child(timer)
 	return timer
 
@@ -13,4 +23,5 @@ static func use_timer(node: Node) -> Timer:
 static func use_tick_timer(node: Node) -> TickTimer:
 	var timer := TickTimer.new()
 	node.add_child(timer)
+	timer.owner = node
 	return timer
